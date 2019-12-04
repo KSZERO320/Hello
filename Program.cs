1813211cfc53b0bcaaebd51feda83dc2d1511c93
KSZERO <19191kuba19191@gmail.com>
2019-12-04 21:43:13 +0100

    1 program, tablica args(parametry wprowadzane przez użytkownika)

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];
+            Console.WriteLine("Hello " + name);
+
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Hello  " + item);
+            //}
+
+        }
+
+    }
+}
